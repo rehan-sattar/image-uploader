68c0b55edd1aaefc81cb88a7dc1daf1cfb2381cc
rehan-sattar <rehansattar117@gmail.com>
2021-09-11 01:35:25 +0500

    🐛 FIX: upload all changes to git, my bad!

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
+const configs = require('./configs');
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+
+configs.loadEnvironmentVariables();
+
 const imageUploadRouter = require('./modules/image-upload/router');
 
 const app = express();
@@ -13,7 +17,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 app.use(
-  '/images',
+  '/uploads',
   express.static(UPLOAD_DIRECTORY_PATH, { maxAge: ONE_YEAR_IN_MILLISECONDS })
 );
 
@@ -22,4 +26,13 @@ app.use(`/api/${API_VERSION}/image-upload`, imageUploadRouter);
 
 app.listen(port, () => {
   console.log('SERVER STARTED AT PORT: ' + port);
+  // Open the database connection
+  configs
+    .establishDatabaseConnection()
+    .then(() => {
+      console.log('Database connection established.');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
