2bf9d912a5a79e5b3ee5637aa7d606833bf5aedd
rehan-sattar <rehansattar117@gmail.com>
2021-09-12 21:31:32 +0500

    📦 NEW: Add basic layout and styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
-import './styles/App.css';
-
 function App() {
-  return <div>Welcome to image-upload</div>;
+  return (
+    <div className="bg-gray-100 h-screen">
+      <div className="h-full w-full flex justify-center items-center">
+        <div className="h-96 w-96 bg-white rounded-lg shadow-md">
+          <div className="h-full flex flex-col py-8 px-6 justify-between items-center">
+            <div className="mx-auto text-center">
+              <h1 className="text-xl">Upload Your Image</h1>
+              <div className="mt-3 text-sm text-gray-500">
+                File should be png or jpg
+              </div>
+            </div>
+            <div className="h-full w-full border-2 rounded border-dashed my-6 bg-gray-100"></div>
+            <div className="text-sm">Or</div>
+            <button className="text-white text-sm hover:bg-blue-300 shadow-md bg-blue-400 rounded py-2 px-4 mt-4">
+              Choose File
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Poppins', 'Helvetica', 'Arial', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
   },
   variants: {
     extend: {},
